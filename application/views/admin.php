5e304ebbb25a028379fe081ec6dc245ebb0d1bb8
Teodor Cataniciu <teodor.cataniciu@gmail.com>
2015-07-21 01:01:03 -0700

    here is the header, footer and adming login

--- application/views/admin.php
+++ application/views/admin.php
@@ -7,18 +7,33 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-   <style type="text/css">
-    body
-    {
-      background-color: #C8D2D9;
-    }
-   </style>
-    <title>Example Title</title>
+   
+    <title>Login</title>
 
     <!-- Bootstrap -->
     <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-
+<style type="text/css">
+    body
+    {
+      background-color: #C5CFD7;
+    }
+    #login{
+    	background-color: #232F3E;
+    	height: auto; 
+    	margin-top: 20%;
+    	color: white;
+    	border-radius: 5px;
+    	padding-top: 40px;
+    	padding-bottom: 30px;
+    	
+    }
+    #img_form{
+    	width: 80%;
+    	border-bottom: 1px solid silver;
+    	margin-bottom: 40px;
+    }
+   </style>
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
@@ -30,11 +45,24 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <![endif]-->
  </head>
  <body>
-<?php  
-include 'application/views/header.php';
-
-include 'application/views/footer.php';
-?>
+  	<div class="container">
+	<div class="row">
+		<div id= "login"class="col-md-5 col-md-offset-3">
+		<center><div id="img_form">
+		<img src="assets/img/company_logo.png" width=200 height=200>
+		</div></center>
+		</img>
+		<form>
+		  <div class="form-group">
+		    <input type="email" name="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+		  </div>
+		  <div class="form-group">
+		    <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+		  </div>
+		  <button type="submit" class="btn btn-default">Submit</button>
+		</form>	
+		</div>
+  	</div>
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 </body>
--- application/views/footer.php
+++ application/views/footer.php
@@ -4,13 +4,36 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <!DOCTYPE html>
 <html lang="en">
   <head>
+    <div id="fb-root"></div>
+  <script>(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4&appId=317453804972856";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+  </script>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
    <style type="text/css">
-    body
-    {
-      background-color: #C8D2D9;
+    .fb-like {
+      vertical-align: top;
+      margin-left: 20px;
+    }
+    .footer
+    {   
+      padding: 8px;
+       border-top: 1px solid silver;
+       padding-bottom: 14px;
+       height: 75px; /* .push must be the same height as .footer */
+      background-color: #F9C402;
+    }
+    .social_media_footer{
+      text-align: center;
+      padding-right: 55px;
+      padding-bottom: 15px;
+     
     }
    </style>
     <title>Example Title</title>
@@ -30,11 +53,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <![endif]-->
  </head>
  <body>
-<?php  
-include 'application/views/header.php';
 
-include 'application/views/footer.php';
-?>
+
+ <div class="footer">
+  <center><i>Made with &lt;3 in Sacramento</i></center>
+  <div class="row">
+  <div class="social_media_footer">
+  <a href="http://fb.com"><img src="assets/img/facebook.png" alt="facebook_pic" height=40px width=40px></a>
+  <a href="http://twitter.com"><img src="assets/img/twitter.png" alt="twitter_pic" height=40px width=40px></a>
+  </div></div></div>
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 </body>

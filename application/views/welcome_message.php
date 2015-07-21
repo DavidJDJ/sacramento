6abab35ef3cfd1ca8b2bec1c6a612ab429752811
David Jimenez <davidjosuedjdj@gmail.com>
2015-07-21 14:23:00 -0700

    latest commit

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -44,6 +44,45 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <![endif]-->
  </head>
  <body>
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<?php
+include 'application/views/header.php';
+
+
+    </div>
+
+include 'application/views/footer.php';
+?>
+>>>>>>> parent of 32b14e7... latest updated on the views
+=======
+    <center><iframe src="assets/slider.html" frameBorder="0"></iframe>
+    <div id="content">
+      <h3>Create a Baltimore Box with an added item from Route One!</h3>
+      <p class="lean">from $40.00</p>
+      <p>The same GREAT Baltimore Box, now with an added item from Route One Apparel! (FREE SHIPPING to USA and Military APO / FPO addresses)</p>
+      <p>Upon clicking "Purchase" below you will be able to select which items you want put inside your Baltimore Box.</p>
+      <p>You get to choose 3, 5 or 10 items for your box from our regular list of Baltimore items. Then you get to select 1 additional item from Route One Apparel!</p>
+       <button type="button" class="btn btn-primary">Add to Cart</button>
+    </div></center>
+    </div>
+<?php include('footer.php');
+=======
+<?php  
+include 'application/views/header.php';
+
+include 'application/views/footer.php';
+>>>>>>> parent of 32b14e7... latest updated on the views
+=======
+<?php  
+include 'application/views/header.php';
+
+include 'application/views/footer.php';
+>>>>>>> parent of 32b14e7... latest updated on the views
+?>
+>>>>>>> parent of c0c043d... all views with empty welcome message submitted
+=======
     <div class="container">
 
     <center><iframe src="assets/slider.html" frameBorder="0"></iframe>
@@ -56,9 +95,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
        <button type="button" class="btn btn-primary">Add to Cart</button>
     </div></center>
     </div>
+<<<<<<< HEAD
+>>>>>>> parent of 107e59c... testing the commits
+=======
 <?php include('footer.php');
 ?>
+>>>>>>> parent of c0c043d... all views with empty welcome message submitted
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 </body>
-</html
+</html>
